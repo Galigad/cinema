a60eeea3e4111058230c32f12432a9ce54a0f4c3
Galigad <artur.szuberla@gmail.com>
2018-05-27 21:13:55 +0200

    First

---
+++ src/main/java/com/cinema/cinemacountry/CinemaCountryApplication.java
@@ -0,0 +1,12 @@
+package com.cinema.cinemacountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaCountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaCountryApplication.class, args);
+    }
+}
