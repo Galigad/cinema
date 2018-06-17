7442a8cdab3bcb830cf0a1ac6aa406f36f192fb4
Galigad <artur.szuberla@gmail.com>
2018-06-17 21:58:32 +0200

    update

---
+++ src/main/java/com/cinema/cinemacountry/ManagementController.java
@@ -0,0 +1,16 @@
+package com.cinema.cinemacountry;
+
+public class ManagementController {
+
+    User user;
+    Admin admin;
+    Repertory repertory;
+
+    Admin createAdminAccount() {
+        return admin;
+    }
+
+    User createUserAccount() {
+        return user;
+    }
+}
