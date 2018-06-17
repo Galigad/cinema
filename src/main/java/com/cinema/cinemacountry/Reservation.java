1767b2a9650123a01eb154183eb5fd5af09fa253
Galigad <artur.szuberla@gmail.com>
2018-06-17 21:56:41 +0200

    up

--- src/main/java/com/cinema/cinemacountry/Reservation.java
+++ src/main/java/com/cinema/cinemacountry/Reservation.java
@@ -1,4 +1,18 @@
 package com.cinema.cinemacountry;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
+@Getter
 public class Reservation {
+    private long id;
+    private LocalDate dateOfCreatedReservation;
+    private Movie movie;
+    private boolean isRealised;
+
+    public Reservation(long id, LocalDate dateOfCreatedReservation, boolean isRealised) {
+        this.id = id;
+        this.dateOfCreatedReservation = dateOfCreatedReservation;
+        this.isRealised = isRealised;
+    }
 }
