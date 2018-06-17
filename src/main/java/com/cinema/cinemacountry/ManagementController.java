5d413e1fa7617005e39f3daa26e4d2a7d8dbd758
Galigad <artur.szuberla@gmail.com>
2018-06-17 19:06:57 +0200

    Update

--- src/main/java/com/cinema/cinemacountry/ManagementController.java
+++ src/main/java/com/cinema/cinemacountry/ManagementController.java
@@ -4,15 +4,13 @@ public class ManagementController {
 
     User user;
     Admin admin;
-    //Repertory repertory;
+    Repertory repertory;
 
-    Admin createAdminAccount(String name, String surname, String mail, int phoneNumber, int pin) {
-        Admin admin = new Admin(name, surname, mail, phoneNumber, pin);
+    Admin createAdminAccount() {
         return admin;
     }
 
-    User createUserAccount(String firstName, String lastName, String email, String phoneNum, int pin) {
-        User user = new User(firstName, lastName, email, phoneNum, pin);
+    User createUserAccount() {
         return user;
     }
 }
