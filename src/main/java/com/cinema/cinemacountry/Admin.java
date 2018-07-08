03fe904d3ce35fdd8317b7c90bf7fe2f749af2ca
walulikt <walulik.t@wp.pl>
2018-07-09 00:15:43 +0200

    Początek implementacji Cinema CountryApplication

--- src/main/java/com/cinema/cinemacountry/Admin.java
+++ src/main/java/com/cinema/cinemacountry/Admin.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("singielton")
 public class Admin {
-    private int id=0;
+    public static int ID=0;
     private String name;
     private String surname;
     private String mail;
@@ -31,5 +31,57 @@ public class Admin {
     public Hall createHall(String hallName, int capacity) { 
         return new Hall(name, capacity);
     }
+    
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public RepertoryConfig getRepertoryConfig() {
+		return repertoryConfig;
+	}
+
+	public void setRepertoryConfig(RepertoryConfig repertoryConfig) {
+		this.repertoryConfig = repertoryConfig;
+	}
+
+
+	
 
 }
--- src/main/java/com/cinema/cinemacountry/CinemaCountryApplication.java
+++ src/main/java/com/cinema/cinemacountry/CinemaCountryApplication.java
@@ -1,12 +1,60 @@
 package com.cinema.cinemacountry;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class CinemaCountryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaCountryApplication.class, args);
+        
+        ApplicationContext context = new AnnotationConfigApplicationContext(ManagementController.class);
+        ManagementController cinemaController = context.getBean(ManagementController.class);  //Choæ nie jest on Beanem?!
+        boolean endProgram = false;
+        Scanner sc= new Scanner(System.in);
+        Admin admin=null;
+        
+        while(endProgram==false) {
+        	System.out.println("Witam w programie Cinema Country. Czy stworzyæ administratora? (T,N)");
+        	String userData = sc.nextLine();
+        	if(userData=="T") {
+        		boolean adminData = false;
+        		while(adminData==false) {
+        			admin = cinemaController.createAdminAccount();
+        			System.out.println("Podaj Imiê: ");
+        			String name = sc.nextLine();
+        			
+        			System.out.println("Podaj nazwiosko:");
+        			String surname = sc.nextLine();
+        			
+        			System.out.println("Podaj nazwiosko:");
+        			int phoneNumber= sc.nextInt();
+        			sc.nextLine();
+        			
+        			System.out.println("Nr telefono ustawiono na: "+ name+". Teraz podaj nazwiosko:");
+        			int adminPin = sc.nextInt();
+        			
+        			System.out.println("Dane Administratora: \n" +" Imiê: " +name+", nazwisko: " + surname +", nr telefonu: " + phoneNumber + '\n'+
+        					"Jeœli dane sie zgadzaj¹ wpisz 'ok'"); 
+        			userData=sc.nextLine();
+        			if(userData=="ok") {
+        				admin.setName(name);
+        				admin.setSurname(surname);
+        				admin.setPhoneNumber(phoneNumber);
+        				admin.setPin(adminPin);
+        				Admin.ID++;
+        				adminData=true;
+        			
+        			} else adminData=false;
+        			/*Nalezy jeszcze dodac obs³ugê wyj¹tków i ladniej bêdzie jak umo¿liwi siê korygowanie danych jeœli u¿ytkownik wprowadzi jakiœ b³¹d.
+        			W celach testowych zamiast Scannera bêdzie wprowadzone FileReader - odczytuj¹cy po kolei dane testowe z pliku)*/
+        		}
+        	}
+        }
     }
 }
